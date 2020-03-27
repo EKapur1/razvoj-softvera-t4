d7926daaf0c51910227a99ca885e5df34940e664
ekapur1 <ekapur1@etf.unsa.ba>
2020-03-27 20:19:37 +0100

    Zadatak 3.

--- src/ba/unsa/etf/rs/tut4/Controller.java
+++ src/ba/unsa/etf/rs/tut4/Controller.java
@@ -1,6 +1,5 @@
 package ba.unsa.etf.rs.tut4;
 
-import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
@@ -13,7 +12,7 @@ public class Controller {
     public Button dugmeDodaj;
     public TextArea ispisiArtikle;
 
-    public void izbaci(ActionEvent actionEvent) {
+    public void izbaci() {
         ArrayList<Artikal> lista = new ArrayList<>();
             String[] s = unesiArtikle.getText().split("\n");
             for(int i = 0; i < s.length; i++){
--- src/ba/unsa/etf/rs/tut4/Racun.java
+++ src/ba/unsa/etf/rs/tut4/Racun.java
@@ -1,10 +1,62 @@
 package ba.unsa.etf.rs.tut4;
 
+import java.util.ArrayList;
+
 public class Racun {
-    public void dodajStavku(Artikal artikal, int i) {
+    private class Stavka{
+        private Artikal artikal;
+        private int kolicina;
+
+        public Stavka(Artikal artikal, int kolicina) {
+            this.artikal = artikal;
+            this.kolicina = kolicina;
+        }
+
+        public Artikal getArtikal() {
+            return artikal;
+        }
+
+        public void setArtikal(Artikal artikal) {
+            this.artikal = artikal;
+        }
+
+        public int getKolicina() {
+            return kolicina;
+        }
+
+        public void setKolicina(int kolicina) {
+            this.kolicina = kolicina;
+        }
     }
 
-    public double ukupanIznos() {
-        return 5;
+    private ArrayList<Stavka> stavke = new ArrayList<>();
+    public Racun() {
+        this.stavke = new ArrayList<>();
     }
+
+    public void dodajStavku(Artikal artikal, int kolicina){
+        Stavka stavka = new Stavka(artikal,kolicina);
+        stavke.add(stavka);
+    }
+
+
+
+    public double ukupanIznos(){
+        double iznos = 0;
+        for (Stavka stavka : stavke) {
+            iznos+= stavka.getArtikal().getCijena() * stavka.getKolicina();
+        }
+        return iznos;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder output = new StringBuilder();
+        for(Stavka stavka: stavke){
+            output.append(stavka.getArtikal().getSifra()).append(" ").append(stavka.getArtikal().getCijena() * stavka.getKolicina()).append("\n");
+        }
+        output.append("Ukupan iznos: ").append(ukupanIznos());
+        return output.toString();
+    }
+
 }
