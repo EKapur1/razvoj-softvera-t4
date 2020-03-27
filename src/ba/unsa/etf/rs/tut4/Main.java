dfb91c09af8832f6ff0dac2ba6b976cd5b79d4e2
ekapur1 <ekapur1@etf.unsa.ba>
2020-03-27 01:33:13 +0100

    Atomic commit

--- src/ba/unsa/etf/rs/tut4/Main.java
+++ src/ba/unsa/etf/rs/tut4/Main.java
@@ -1,10 +1,21 @@
 package ba.unsa.etf.rs.tut4;
 
-import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class Main {
-    public static void main(String[] args) {
-
-
+public class Main extends Application{
+        @Override
+        public void start(Stage primaryStage) throws Exception{
+            Parent root = FXMLLoader.load(getClass().getResource("Tutorijal3-zad2.fxml"));
+            primaryStage.setTitle("Hello World");
+            primaryStage.setScene(new Scene(root, 300, 275));
+            primaryStage.show();
     }
+        public static void main(String[] args) {
+            launch(args);
+        }
+
 }
