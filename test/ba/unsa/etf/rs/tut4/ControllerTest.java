95a29665cb6835edac2564ecafdc4bce1fa2190c
ekapur1 <ekapur1@etf.unsa.ba>
2020-04-05 13:35:56 +0200

    Controller 75% pokriven testovima

--- test/ba/unsa/etf/rs/tut4/ControllerTest.java
+++ test/ba/unsa/etf/rs/tut4/ControllerTest.java
@@ -1,8 +1,15 @@
 package ba.unsa.etf.rs.tut4;
 
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+import static org.junit.jupiter.api.Assertions.*;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,9 +17,7 @@ import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
 
-import java.awt.*;
-
-import static org.junit.Assert.assertNotNull;
+import java.util.ArrayList;
 
 @ExtendWith(ApplicationExtension.class)
 class ControllerTest {
@@ -25,5 +30,30 @@ class ControllerTest {
             stage.toFront();
         }
 
+    public TextArea unesiArtikle;
+    public Button dugmeDodaj;
+    public TextArea ispisiArtikle;
+    public ChoiceBox<String> liste;
+    public Spinner<Integer> spin;
+    public Button dodaj;
+    public TextArea ispisiRacun;
+    Racun racun;
+    public ArrayList<Artikal> artikli;
+
+    @Test
+    public void testButtonClick (FxRobot robot) {
+        TextArea t2 = robot.lookup("#ispisiArtikle").queryAs(TextArea.class);
+        TextArea t3 = robot.lookup("#ispisiRacun").queryAs(TextArea.class);
+        robot.clickOn("#unesiArtikle");
+        robot.write("HLB001,Crni hljeb,1\n" +
+                "HLB002,Bijeli hljeb,1.10\n" +
+                "HLB002,Bijeli hljeb,1.10\n" +
+                "MLK001,Mlijeko Meggle,1.10");
+        robot.clickOn("#dugmeDodaj");
+        assertEquals("HLB001,Crni hljeb,1.0\n" +
+                "HLB002,Bijeli hljeb,1.1\n" +
+                "MLK001,Mlijeko Meggle,1.1\n", t2.getText());
+
+    }
 
 }
